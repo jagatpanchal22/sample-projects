e583e437dd1852ac20fd549e097e7caf2ff3d433
Jagat Panchal <phenix@phenix-Lenovo-G50-80>
2021-05-29 00:17:24 +0530

    Order Management System

--- company/company/asgi.py
+++ company/company/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for OrderManagementSystem project.
+ASGI config for company project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OrderManagementSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'company.settings')
 
 application = get_asgi_application()
--- company/company/wsgi.py
+++ company/company/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for OrderManagementSystem project.
+WSGI config for company project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OrderManagementSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'company.settings')
 
 application = get_wsgi_application()
